b728c43f2503f5dcc3ab43ce7543f4b33990618b
Subhash Barela <barelasubhash16@gmail.com>
2023-06-25 14:55:23 +0530

    [subhash] Check Part time Or full Time Emp

--- Day1_EmpWage/EmployeeCheck.cs
+++ Day1_EmpWage/EmployeeCheck.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -41,6 +43,39 @@ namespace Day1_EmpWage
             }
             empWage = empHrs * Emp_Rate_Per_Hour;
             Console.WriteLine("Employee Wage is :" + empWage);
+        }
+        public void PartTime_FullTime_Emp()
+        {
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            const int FULL_TIME_HR = 8;
+            const int PART_TIME_HR = 4;
+            const int WAGE_PER_HR = 20;
+             int empHrs = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0,3);
+
+            switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    {
+                        Console.WriteLine("Full Time Employee");
+                        empHrs = FULL_TIME_HR;
+                        break;
+                    }
+                case IS_PART_TIME:
+                    {
+                        Console.WriteLine("Part Time Employee");
+                        empHrs = PART_TIME_HR;
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Employee is absent");
+                    empHrs = 0;
+                    break;
+            }
+            int empWage = empHrs * WAGE_PER_HR;
+            Console.WriteLine("EmpWage Is : " + empWage);
         }
     }
 }
--- Day1_EmpWage/Program.cs
+++ Day1_EmpWage/Program.cs
@@ -12,22 +12,30 @@ namespace Day1_EmpWage
         static void Main(string[] args)
         {
             EmployeeCheck emp = new EmployeeCheck();
-
-            Console.WriteLine("1:Check Emp Avalability \n2: Check Daily Wage");
-            Console.WriteLine("Enter the choice");
-            int choice=int.Parse(Console.ReadLine());
-            switch(choice)
+            string ans = "";
+            do
             {
-                case 1:
-                    emp.CheckEmp();
-                    break;
-                case 2 :
-                    emp.DailyWage();
-                    break;
-                default:
-                    Console.WriteLine("Please insert valid number");
-                    break;
-            }
+                Console.WriteLine(" 1:Check Emp Avalability \n 2: Check Daily Wage \n 3: PartTime_FullTime_Emp");
+                Console.WriteLine("Enter the choice");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        emp.CheckEmp();
+                        break;
+                    case 2:
+                        emp.DailyWage();
+                        break;
+                    case 3:
+                        emp.PartTime_FullTime_Emp();
+                        break;
+                    default:
+                        Console.WriteLine("Please insert valid number");
+                        break;
+                }
+                Console.WriteLine("Do you want to Continue ?");
+                ans = Console.ReadLine();
+            } while (ans == "yes" || ans == "y");
         }
     }
 }
