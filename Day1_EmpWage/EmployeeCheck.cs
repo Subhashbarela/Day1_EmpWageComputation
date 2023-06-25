4cb34164258131916eef1acb618853d0ffefea61
Subhash Barela <barelasubhash16@gmail.com>
2023-06-25 15:06:34 +0530

    [subhash] check part time full time emp using switch case

--- Day1_EmpWage/EmployeeCheck.cs
+++ Day1_EmpWage/EmployeeCheck.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -10,7 +11,7 @@ namespace Day1_EmpWage
 {
     internal class EmployeeCheck
     {
-        public  void CheckEmp()
+        public void CheckEmp()
         {
             int Emp_is_present = 1;
             Random random = new Random();
@@ -24,7 +25,7 @@ namespace Day1_EmpWage
                 Console.WriteLine("employee is absent");
             }
         }
-        public  void DailyWage()
+        public void DailyWage()
         {
             int Is_Full_Time = 1;
             int Emp_Rate_Per_Hour = 20;
@@ -51,9 +52,42 @@ namespace Day1_EmpWage
             const int FULL_TIME_HR = 8;
             const int PART_TIME_HR = 4;
             const int WAGE_PER_HR = 20;
-             int empHrs = 0;
+            int empHrs = 0;
             Random random = new Random();
-            int empCheck = random.Next(0,3);
+            int empCheck = random.Next(0, 3);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+
+                Console.WriteLine("Full Time Employee");
+                empHrs = FULL_TIME_HR;
+
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Part Time Employee");
+                empHrs = PART_TIME_HR;
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                empHrs = 0;
+
+            }
+            int empWage = empHrs * WAGE_PER_HR;
+            Console.WriteLine("EmpWage Is : " + empWage);
+        }
+        public void PartTime_FullTime_Emp_Using_SwitchCase()
+        {
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            const int FULL_TIME_HR = 8;
+            const int PART_TIME_HR = 4;
+            const int WAGE_PER_HR = 20;
+            int empHrs = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
 
             switch (empCheck)
             {
--- Day1_EmpWage/Program.cs
+++ Day1_EmpWage/Program.cs
@@ -15,7 +15,7 @@ namespace Day1_EmpWage
             string ans = "";
             do
             {
-                Console.WriteLine(" 1:Check Emp Avalability \n 2: Check Daily Wage \n 3: PartTime_FullTime_Emp");
+                Console.WriteLine(" 1:Check Emp Avalability \n 2: Check Daily Wage \n 3: PartTime_FullTime_Emp \n 4: PartTime FullTime Emp Using SwitchCase");
                 Console.WriteLine("Enter the choice");
                 int choice = int.Parse(Console.ReadLine());
                 switch (choice)
@@ -28,6 +28,9 @@ namespace Day1_EmpWage
                         break;
                     case 3:
                         emp.PartTime_FullTime_Emp();
+                        break;
+                    case 4:
+                        emp.PartTime_FullTime_Emp_Using_SwitchCase();
                         break;
                     default:
                         Console.WriteLine("Please insert valid number");
