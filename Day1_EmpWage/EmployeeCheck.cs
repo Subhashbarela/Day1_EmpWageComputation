b780277be34c67b4cdd277680d7ebbdff7837f06
Subhash Barela <barelasubhash16@gmail.com>
2023-06-25 14:31:32 +0530

    [subhash] check employee is present or absent

---
+++ Day1_EmpWage/EmployeeCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_EmpWage
+{
+    internal class EmployeeCheck
+    {
+        public  void CheckEmp()
+        {
+            int Emp_is_present = 1;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck == Emp_is_present)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+}
--- Day1_EmpWage/Program.cs
+++ Day1_EmpWage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,20 @@ namespace Day1_EmpWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            EmployeeCheck emp = new EmployeeCheck();
+
+            Console.WriteLine("1:Check Emp Avalability");
+            Console.WriteLine("Enter the choice");
+            int choice=int.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    emp.CheckEmp();
+                    break;
+                default:
+                    Console.WriteLine("Please insert valid number");
+                    break;
+            }
         }
     }
 }
